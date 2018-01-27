42fedcc8053a7c57ad22711cc3e18fc4c3841f7a
GLOBAL GAME JAM <gamejam@grupomarista.local>
2018-01-27 01:41:18 -0200

    jump fixed

--- Assets/Scripts/StrongCharacter.cs
+++ Assets/Scripts/StrongCharacter.cs
@@ -8,19 +8,30 @@ public class StrongCharacter : MonoBehaviour{
     public LayerMask groundMask;
     public Transform groundCheck;
     private Rigidbody2D rb;
+    private bool canJump;
 
 
     private void Start() {
          rb = GetComponent<Rigidbody2D>();
+         canJump = false;
     }
 
-    public void FixedUpdate() {
-        
-        if(Physics2D.Linecast(transform.position, groundCheck.position, 1 << groundMask, 1.0f)){
-            Debug.Log("can jump");
-            if(Input.GetAxis("Fire1") != 0){
+    public void Update() {
+        if(isGrounded()){
+            if(Input.GetAxis("Fire1") != 0 && canJump){
                 rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+                canJump = false;
             }
         }
+    }
+
+    public bool isGrounded()
+    {
+        bool result = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (!result)
+            canJump = true;
+          
+        return result;
     }
 }
