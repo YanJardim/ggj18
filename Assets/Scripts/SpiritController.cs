6e0c0321a4b829d2361028072c60fecf1ece461e
Bruno Freitas Serbena <bruno.serbena@gmail.com>
2018-01-27 02:28:08 -0200

    Identation

--- Assets/Scripts/SpiritController.cs
+++ Assets/Scripts/SpiritController.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
 public class SpiritController : Character {
-   
+
     public void FixedUpdate() {
-        
-        transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 
+
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * speed * Time.deltaTime,
                                           Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
 
     }
