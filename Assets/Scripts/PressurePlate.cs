af138b8b467e59a4da3dedd7af69eec281ae9776
GLOBAL GAME JAM <gamejam@grupomarista.local>
2018-01-27 03:52:16 -0200

    PressurePlate FallingBox KillingPlayer

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour {
+
+    // Use this for initialization
+    public bool activated;
+	void Start () {
+        activated = false;
+	}
+	
+	// Update is called once per frame
+	public void FixedUpdate () {
+	}
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Box") {
+            Debug.Log("enter");
+            activated = true;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Box") {
+            Debug.Log("exit");
+            activated = false;
+        }
+    }
+}
