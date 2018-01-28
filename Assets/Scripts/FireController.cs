9db1cd7f8af7e2706d8c5e20ab557a08815dfc68
Yan Jardim <yan.guilherme72@gmail.com>
2018-01-28 13:44:21 -0200

    FOGO

--- Assets/Scripts/FireController.cs
+++ Assets/Scripts/FireController.cs
@@ -4,12 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FireController : Timer {
+	public List<GameObject> childrens;
+
+	private void Start() {
+		foreach(Transform c in transform) {
+			childrens.Add(c.gameObject);
+		}
+		SetFogosOnline(false);
+	}
+
 	public override void Activate() {
 		startTimer();
+		SetFogosOnline(true);
 	}
 
 	public override void Deactivate() {
 		stopTimer();
+		SetFogosOnline(false);
 	}
 
 	public override void firstAction() {
@@ -26,5 +37,10 @@ public class FireController : Timer {
 		Debug.Log("Activated");
 	}
 
+	public void SetFogosOnline(bool foguinho) {
+		foreach (GameObject a in childrens) {
+			a.SetActive(foguinho);
+		}
+	}
 
 }
