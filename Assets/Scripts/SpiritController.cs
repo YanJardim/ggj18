f17dfd2cbf0c3690a1efb71e97d0f6a0269d77e9
Yan Jardim <yan.guilherme72@gmail.com>
2018-01-28 02:25:37 -0200

    Add some character animations

--- Assets/Scripts/SpiritController.cs
+++ Assets/Scripts/SpiritController.cs
@@ -1,66 +1,67 @@
 using UnityEngine;
 
 public class SpiritController : CharacterControllerBase {
-    public float depossessSpeed;
+	public float depossessSpeed;
 
-    public float possessionCooldown;
-    [HideInInspector]
-    public float possessionCD;
+	public float possessionCooldown;
+	[HideInInspector]
+	public float possessionCD;
 
-    protected Collider2D col;
+	protected Collider2D col;
 
-    public void Awake() {
-        col = GetComponent<Collider2D>();
-    }
+	public void Awake() {
+		col = GetComponent<Collider2D>();
+	}
 
-    public new void Start() {
-        base.Start();
+	public new void Start() {
+		base.Start();
 
-        GameManager.Instance.spirit = this.gameObject;
-    }
-    public bool Cooldown(ref float CD) {
-        if (CD > 0) {
-            CD -= Time.deltaTime;
+		GameManager.Instance.spirit = this.gameObject;
+	}
+	public bool Cooldown(ref float CD) {
+		if (CD > 0) {
+			CD -= Time.deltaTime;
 
-            if (CD <= 0f) {
-                CD = 0f;
-                return true;
-            }
-        }
-        return false;
-    }
+			if (CD <= 0f) {
+				CD = 0f;
+				return true;
+			}
+		}
+		return false;
+	}
 
-    public new void Update() {
-        if(Cooldown(ref possessionCD)) {
-            col.enabled = true;
-        }
-        if(possessionCD > 0) {
-            rb.AddForce(Vector2.up * depossessSpeed * (possessionCD / possessionCooldown));
-        }
+	public new void Update() {
+		if (Cooldown(ref possessionCD)) {
+			col.enabled = true;
+		}
+		if (possessionCD > 0) {
+			rb.AddForce(Vector2.up * depossessSpeed * (possessionCD / possessionCooldown));
+		}
 
-    }
+	}
 
-    public void OnEnable() {
-        if (rb != null)
-            rb.AddForce(Vector2.up * 20);
-            //rb.velocity = new Vector2(0f, 1000f);
+	public void OnEnable() {
+		if (rb != null)
+			rb.AddForce(Vector2.up * 20);
+		//rb.velocity = new Vector2(0f, 1000f);
 
-        possessionCD = possessionCooldown;
-        if(col != null)
-            col.enabled = false;
-    }
+		possessionCD = possessionCooldown;
+		if (col != null)
+			col.enabled = false;
+	}
 
-    public new void FixedUpdate() {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed,
-                                    Input.GetAxis("Vertical") * speed);
-    }
+	public new void FixedUpdate() {
+		rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed,
+									Input.GetAxis("Vertical") * speed);
+	}
 
-    void OnCollisionEnter2D(Collision2D other) {
-        if(possessionCD > 0) {
-            return;
-        }
-        if (other.gameObject.tag == "Character") {
-            gameObject.SetActive(false);
-        }
-    }
+	void OnCollisionEnter2D(Collision2D other) {                               
+		Debug.Log("collided");
+		if (possessionCD > 0) {
+			return;
+		}
+		if (other.gameObject.tag == "Character") {
+			gameObject.SetActive(false);
+		}
+	}
 }
