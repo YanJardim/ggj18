8bd8c3e6f14e94789f409902ebf23ea4e9658f8a
GLOBAL GAME JAM <gamejam@grupomarista.local>
2018-01-28 00:25:38 -0200

    added lever system,fixed altar actvation system

--- Assets/Scripts/Altar.cs
+++ Assets/Scripts/Altar.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class Altar : MonoBehaviour {
     public int uses;
+	private CharacterControllerBase character;
 
-    public bool Use() {
+	private bool isOnArea = false;
+
+	void Update() {
+		if (character && isOnArea && Input.GetKeyDown(KeyCode.E) && Use()) {
+			character.Depossess();
+		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Character")
+			character = other.GetComponent<CharacterControllerBase>();
+
+		isOnArea = true;
+		Debug.Log("On Altar");
+	}
+	public void OnTriggerExit2D(Collider2D other) { 
+		if (other.gameObject.tag == "Character")
+			character = null;
+		isOnArea = false;
+		Debug.Log("Leave altar");
+	}
+
+	public bool Use() {
         if (uses >= 1) {
             uses--;
             return true;
