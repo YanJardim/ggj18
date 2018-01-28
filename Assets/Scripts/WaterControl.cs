8f62ba182d40f7c1c9ad62f1b5f580ee76963119
GLOBAL GAME JAM <gamejam@grupomarista.local>
2018-01-28 06:51:43 -0200

    Level 2 in progress

---
+++ Assets/Scripts/WaterControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterControl : Activatable {	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void Activate() {
+		foreach (Transform child in transform) {
+			child.gameObject.SetActive(true);
+		}
+	}
+
+	public override void Deactivate() {
+		foreach (Transform child in transform) {
+			child.gameObject.SetActive(false);
+		}
+	}
+
+	
+}
